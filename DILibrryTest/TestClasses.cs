a7d2cc0dd615d2129ba8688bcf88a30324ce5f66
darya-cherr <chernikovadarya67@gmail.com>
2022-02-07 05:10:11 +0300

    Added test with registration types

---
+++ DILibrryTest/TestClasses.cs
@@ -0,0 +1,152 @@
+﻿namespace DILibrryTest
+{
+    public static class TestClasses
+    {
+        public interface IService {}
+
+        public class Service1 : IService {}
+
+        public class Service2 : IService {}
+    
+        public abstract class AbstractService : IService {}
+    
+        public class AbstractServiceImpl : AbstractService {}
+    
+        public class Service3 : IService
+        {
+            public IRepository Repository { get; set; }
+
+            public Service3(IRepository repository)
+            {
+                Repository = repository;
+            }
+        }
+
+        public interface IRepository{}
+
+        public class Repository1 : IRepository {}
+
+        public interface IService<TRepository> where TRepository : IRepository {}
+
+        public class Service4<TRepository> : IService<TRepository> where TRepository : IRepository
+        {
+            public TRepository Repository { get; set; }
+            public Service4(TRepository repository)
+            {
+                Repository = repository;
+            }
+        }
+        
+        public class Service5 : IService
+        {
+            public Service5(Service2 service2)
+            {
+                
+            }
+            public IRepository Repository { get; set; }
+        }
+        
+        public interface IA { }
+
+        public class A : IA
+        {
+            public IB b { get; set; }
+
+            
+            public A(IB b)
+            {
+                this.b = b;
+            }
+
+            public A()
+            {
+                
+            }
+            
+            
+        }
+        
+        public interface IB {}
+
+        public class B : IB
+        {
+            public IA a { get; set; }
+            public IC c { get; set; }
+
+
+            public B(IA a, IC c)
+            {
+                this.a = a;
+                this.c = c;
+            }
+
+            public B() {}
+        }
+        
+        public interface IC {}
+
+        public class C : IC
+        {
+            public IB b { get; set; }
+
+            
+            public C(IB b)
+            {
+                this.b = b;
+            }
+
+            public C() {}
+        }
+        
+        
+        public interface IQ { }
+
+        public class Q : IQ
+        {
+            public IW w { get; set; }
+
+            
+            public Q(IW w)
+            {
+                this.w = w;
+            }
+
+            public Q()
+            {
+                
+            }
+            
+            
+        }
+        
+        public interface IW {}
+
+        public class W : IW
+        {
+            public IE e { get; set; }
+
+            
+            public W(IE e)
+            {
+                this.e = e;
+            }
+
+            public W() {}
+        }
+        
+        public interface IE {}
+
+        public class E : IE
+        {
+            public IQ q { get; set; }
+
+            
+            public E(IQ q)
+            {
+                this.q = q;
+            }
+
+            public E() {}
+        }
+    }
+}
--- DILibrryTest/UnitTest1.cs
+++ DILibrryTest/UnitTest1.cs
@@ -1,15 +1,31 @@
+using DependencyInjection;
 using NUnit.Framework;
+using static DILibrryTest.TestClasses;
 
 public class Tests
 {
+    private DependencyConfiguration _configuration;
+    
     [SetUp]
     public void Setup()
     {
+        _configuration = new DependencyConfiguration();
     }
 
     [Test]
-    public void Test1()
+    public void ResolveBasicTypes()
     {
-        Assert.Pass();
+        _configuration.RegisterSingleton<IService, Service1>();
+        _configuration.RegisterTransient<AbstractService, AbstractServiceImpl>();
+            
+        var container = _configuration.GenerateContainer();
+            
+        var service = container.GetService<IService>();
+        var serviceFirst = container.GetService<AbstractService>();
+        Assert.AreEqual(typeof(Service1), service.GetType());
+        Assert.AreEqual(typeof(AbstractServiceImpl), serviceFirst.GetType());
     }
+    
+    
+    
 }
